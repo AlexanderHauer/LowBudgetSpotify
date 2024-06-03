d80687add03c2b03efdd053f9898594b5099b64d
AlexanderHauer <162414428+AlexanderHauer@users.noreply.github.com>
2024-06-03 13:14:59 +0300

    Add files via upload

---
+++ LowBudgetSpotify_ProgrammingProject-main/playlist.h
@@ -0,0 +1,28 @@
+
+#ifndef LOWBUDGETSPOTIFY_PLAYLIST_H
+#define LOWBUDGETSPOTIFY_PLAYLIST_H
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <list>
+#include <vector>
+#include "song.h"
+#include "app.h"
+#include "utils.h"
+
+extern std::list<class Playlist> globalPlaylistList;
+
+class Playlist {
+public:
+    std::string name;
+    std::string songString;
+};
+
+void loadPlaylistFromTXT();
+void createPlaylist(const std::string& name, const std::string& surname);
+void searchPlaylist(const std::string& name, const std::string& surname);
+void addPlaylistToTXT(const std::string& playlistName, const std::string& songString);
+
+#endif //LOWBUDGETSPOTIFY_PLAYLIST_H
---
+++ LowBudgetSpotify_ProgrammingProject-main/song.h
@@ -0,0 +1,27 @@
+
+#ifndef LOWBUDGETSPOTIFY_SONG_H
+#define LOWBUDGETSPOTIFY_SONG_H
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <list>
+#include "app.h"
+#include "utils.h"
+
+extern std::list<class Song> globalSongList;
+
+class Song {
+public:
+    std::string artistName;
+    std::string songName;
+};
+
+void loadSongFromTXT();
+void searchSong(const std::string& name, const std::string& surname);
+void uploadSong(const std::string& name, const std::string& surname);
+void searchArtist(const std::string& name, const std::string& surname);
+void addSongToTXT(const std::string& artistName, const std::string& songName);
+
+#endif //LOWBUDGETSPOTIFY_SONG_H
